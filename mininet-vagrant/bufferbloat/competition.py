682da7df9c5ebae4861f30a7cd3680a202e78c52
Gabriel <gabriel.clmartins@gmail.com>
2025-07-18 23:32:39 -0300

    corrigindo throughput

--- mininet-vagrant/bufferbloat/competition.py
+++ mininet-vagrant/bufferbloat/competition.py
@@ -1,3 +1,4 @@
+# competition.py
 from mininet.topo import Topo
 from mininet.node import CPULimitedHost
 from mininet.link import TCLink
@@ -19,7 +20,6 @@ import sys
 import os
 import math
 
-
 import random
 import numpy as np
 import matplotlib.pyplot as plt
@@ -29,106 +29,65 @@ parser.add_argument('--bwhost', '-B',
                     type=float,
                     help="Bandwidth of host links (Mb/s)",
                     default=1000)
-
 parser.add_argument('--bwnet', '-b',
                     type=float,
                     help="Bandwidth of bottleneck (network) link (Mb/s)",
                     required=True)
-
-parser.add_argument('--delay', 
+parser.add_argument('--delay',
                     type=float,
                     help="Link propagation delay (ms)",
                     required=True)
-
 parser.add_argument('--dir', '-d',
                     help="Directory to store outputs",
                     required=True)
-
 parser.add_argument('--time', '-t',
                     help="Duration (sec) to run the experiment",
                     type=int,
                     default=10)
-
 parser.add_argument('--maxq',
                     type=int,
                     help="Max buffer size of network interface in packets",
                     default=100)
-
-parser.add_argument('--reno', 
-                     type=int,
-                     help='Amount of TCP Reno flows',
-                     default=2
-                     )
-
-
+parser.add_argument('--reno',
+                    type=int,
+                    help='Amount of TCP Reno flows',
+                    default=2)
 parser.add_argument('--bbr',
-                     type=int,
-                     help='Amount of TCP BBR flows',
-                     default=2
-                     )
+                    type=int,
+                    help='Amount of TCP BBR flows',
+                    default=2)
 
-
-
-
-# Expt parameters
 args = parser.parse_args()
-
 os.makedirs(args.dir, exist_ok=True)
 
 class BBTopo(Topo):
     "Simple topology for bufferbloat experiment."
 
     def build(self, n=2):
-        # TODO: create two hosts
-
         self.renos = []
         self.bbrs = []
 
         for i in range(args.reno):
-            self.renos.append(self.addHost(f'r{i}')) # Computadores domésticos para fluxo reno
+            self.renos.append(self.addHost(f'r{i}'))
 
         for i in range(args.bbr):
-            self.bbrs.append(self.addHost(f'b{i}')) # Computadores domésticos para fluxo bbr
+            self.bbrs.append(self.addHost(f'b{i}'))
 
-        s = self.addHost('s') # Servidor
-
-
-        # Here I have created a switch.  If you change its name, its
-        # interface names will change from s0-eth1 to newname-eth1.
+        s = self.addHost('s')
         roteador = self.addSwitch('s0')
 
-        self.addLink('s', roteador, # Definindo a implementação da fila na
-                                    # interface de rede do servidor (todos os hospedeiros vão competir por essa fila)
-                    bw=args.bwnet, 
-                    delay="%sms" % args.delay, 
-                    max_queue_size=args.maxq,
-                    use_htb=True)
+        # -- Link de gargalo: usando TBF (shape estrito), sem bursts inesperados --
+        self.addLink('s', roteador,
+                     bw=args.bwnet,
+                     delay=f"{args.delay}ms",
+                     max_queue_size=args.maxq)
 
-
-
-        # TODO: Add links with appropriate characteristics
-        for i in self.renos:
-
-            self.addLink(i, roteador, 
-                        bw=args.bwhost, 
-                        delay="%sms" % args.delay,
-                        use_htb=True)
-
-        for i in self.bbrs:
-
-            self.addLink(i, roteador, 
-                        bw=args.bwhost, 
-                        delay="%sms" % args.delay,
-                        use_htb=True)
-
-
-
-        print(roteador)
-
-
-# Simple wrappers around monitoring utilities.  You are welcome to
-# contribute neatly written (using classes) monitoring scripts for
-# Mininet!
+        # links dos hosts clientes (Reno e BBR) ao roteador
+        for i in self.renos + self.bbrs:
+            self.addLink(i, roteador,
+                         bw=args.bwhost,
+                         delay=f"{args.delay}ms",
+                         use_htb=True)
 
 def getRenos(net):
     return [net.get(f"r{i}") for i in range(args.reno)]
@@ -146,50 +105,34 @@ def getTipo(host):
         return "bbr"
     else:
         return "servidor"
-    
+
 def getHospedeiros(net):
     return getRenos(net) + getBBRs(net)
 
 def nomeArquivo(cliente):
-
-
     tipo = getTipo(cliente)
-
     numero = int(''.join(filter(str.isdigit, cliente.name)))
-
     return f'{tipo}{numero}'
-
 
 def start_iperf(net):
     s = net.get('s')
     hospedeiros = getHospedeiros(net)
 
-
     print("Starting iperf server...")
-    # For those who are curious about the -w 16m parameter, it ensures
-    # that the TCP flow is not receiver window limited.  If it is,
-    # there is a chance that the router buffer may not get filled up.
     servidor = s.popen("iperf -s -w 16m")
 
-    # TODO: Start the iperf client on h1.  Ensure that you create a
-    # long lived TCP flow.
     proc_clientes = []
     arquivos = []
     for cliente in hospedeiros:
         arquivo = open(f"{args.dir}/{nomeArquivo(cliente)}-vazao.txt", "w")
         arquivos.append(arquivo)
-        proc_clientes.append(cliente.popen(f"iperf -c {s.IP()} -t {args.time} -i 1",
+        # -O 2: pula 2s de aquecimento; -i 0.5: janelas de 0.5s
+        cmd = f"iperf -c {s.IP()} -t {args.time} -O 2 -i 0.5 -w 16m"
+        proc_clientes.append(cliente.popen(cmd,
                                            stdout=arquivo,
-                                           universal_newlines=True
-                                           
-                                           ))
-
-
+                                           universal_newlines=True))
     print("iperf server running successfully!")
-
-
     return servidor, proc_clientes, arquivos
-
 
 def start_qmon(iface, interval_sec=0.1, outfile=f"{args.dir}/q.txt"):
     monitor = Process(target=monitor_qlen,
@@ -198,208 +141,87 @@ def start_qmon(iface, interval_sec=0.1, outfile=f"{args.dir}/q.txt"):
     return monitor
 
 def start_ping(h1, h2, ping_file):
-    # TODO: Start a ping train from h1 to h2 (or h2 to h1, does it
-    # matter?)  Measure RTTs every 0.1 second.  Read the ping man page
-    # to see how to do this.
-
-    # pings
-
-
     info(f"* Starting ping train {h1}->{h2}...\n")
-    ping_c_to_s = h1.popen(f"ping -D -i 0.1 {h2.IP()} > {ping_file}", shell=True) 
-
-
-    return ping_c_to_s
-
-    # Hint: Use host.popen(cmd, shell=True).  If you pass shell=True
-    # to popen, you can redirect cmd's output using shell syntax.
-    # i.e. ping ... > /path/to/ping.
-    
+    return h1.popen(f"ping -D -i 0.1 {h2.IP()} > {ping_file}", shell=True)
 
 def start_webserver(servidor):
-
     info(f"* Starting webserver on {servidor}...\n")
     proc = servidor.popen("python webserver.py", shell=True)
     sleep(1)
     print(f"webpage ready: {proc}")
     return [proc]
 
-
-
 def pegar_pagina(net, clientes, tempos):
-        servidor = getServidor(net)
-        pagina_local = f'{args.dir}/web.txt'
-        endereco_servidor = f'http://{servidor.IP()}:8080/index.html'
+    servidor = getServidor(net)
+    pagina_local = f'{args.dir}/web.txt'
+    endereco_servidor = f'http://{servidor.IP()}:8080/index.html'
 
-        # Lança todos os processos antes
-        processos = []
-        tipos = []
+    processos = []
+    tipos = []
+    for cliente in clientes:
+        tipo = getTipo(cliente)
+        proc = cliente.popen(f"curl -o {pagina_local} -w %{{time_total}} -s {endereco_servidor}",
+                             stdout=PIPE, shell=True)
+        stats = cliente.cmd("ss -ti dst %s" % servidor.IP())
+        match = re.search(r'retrans:\s*(\d+)', stats)
+        perdas = int(match.group(1)) if match else 0
+        with open(f"{args.dir}/{nomeArquivo(cliente)}-perdas.txt", "a") as f:
+            f.write(f"{perdas}\n")
+        processos.append(proc)
+        tipos.append(tipo)
 
-        for cliente in clientes:
-            tipo = getTipo(cliente)
-            proc = cliente.popen(f"curl -o {pagina_local}  -w %{{time_total}} -s {endereco_servidor}", stdout=PIPE)
-            stats = cliente.cmd("ss -ti dst %s" % servidor.IP())
-            match = re.search(r'retrans:\s*(\d+)', stats)
-            if match:
-                perdas = int(match.group(1))
-            else:
-                perdas = 0
-
-            nome = nomeArquivo(cliente)
-            with open(f"{args.dir}/{nome}-perdas.txt", "a") as f:
-                f.write(f"{perdas}\n")
-
-            processos.append(proc)
-            tipos.append(tipo)
-
-        # Aguarda todos os processos juntos
-        for proc, tipo in zip(processos, tipos):
-            output = float(proc.communicate()[0].decode().strip())
-            tempos[tipo].append(output)
-
-
+    for proc, tipo in zip(processos, tipos):
+        output = float(proc.communicate()[0].decode().strip())
+        tempos[tipo].append(output)
 
 def competition():
-    if not os.path.exists(args.dir):
-        os.makedirs(args.dir)
-
-
-
     topo = BBTopo()
-    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink) 
-    net.start()    
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+    net.start()
 
     renos = getRenos(net)
     bbrs = getBBRs(net)
     clientes = getHospedeiros(net)
     servidor = getServidor(net)
 
-    # Set TPC CC algorith
-    for host in renos:
-        host.cmd("sysctl -w net.ipv4.tcp_congestion_control=reno")
+    # Define CC
+    for h in renos:
+        h.cmd("sysctl -w net.ipv4.tcp_congestion_control=reno")
+    for h in bbrs:
+        h.cmd("sysctl -w net.ipv4.tcp_congestion_control=bbr")
 
-    for host in bbrs:
-        host.cmd("sysctl -w net.ipv4.tcp_congestion_control=bbr")
-
-
-    # This dumps the topology and how nodes are interconnected through
-    # links.
     dumpNodeConnections(net.hosts)
-
-    # This performs a basic all pairs ping test.
     net.pingAll()
 
+    # Inicia monitor de fila (escolha a interface correta)
+    iface = net.get('s0').connectionsTo(servidor)[0][0].name
+    qmon = start_qmon(iface=iface, outfile=f'{args.dir}/q.txt')
 
-    # TODO: Start monitoring the queue sizes.  Since the switch I
-    # created is "s0", I monitor one of the interfaces.  Which
-    # interface?  The interface numbering starts with 1 and increases.
-    # Depending on the order you add links to your network, this
-    # number may be 1 or 2.  Ensure you use the correct number.
-    qmon = start_qmon(iface=net.get('s0').connectionsTo(servidor)[0][0].name,
-                      outfile=f'{args.dir}/q.txt'
-                      )
-
-    
-
-    # TODO: Start iperf, webservers, etc.
-    iperf_servidor, iperf_clientes, iperf_arquivos = start_iperf(net) # Conexão tcp contínua
+    iperf_servidor, iperf_clientes, iperf_arquivos = start_iperf(net)
     web = start_webserver(servidor)
+    pings = [start_ping(c, servidor, f'{args.dir}/{nomeArquivo(c)}-ping.txt')
+             for c in clientes]
 
-
-    # TODO: measure the time it takes to complete webpage transfer
-    # from h1 to h2 (say) 3 times.  Hint: check what the following
-    # command does: curl -o /dev/null -s -w %{time_total} google.com
-    # Now use the curl command to fetch webpage from the webserver you
-    # spawned on host h1 (not from google!)
-    # reno
-    # Hint: Verify the url by running your curl command without the
-    # flags. The html webpage should be returned as the response.
-   
-
-    
-    # Hint: have a separate function to do this and you may find the
-    # loop below useful.
-    pings = [start_ping(cliente, servidor, f'{args.dir}/{nomeArquivo(cliente)}-ping.txt') for cliente in clientes]
-
-    tempos = dict()
-    tempos['bbr'] = []
-    tempos['reno'] = []
-
-    amostras_por_grupo = 30 # Análise estatística robusta
-
+    tempos = {'bbr': [], 'reno': []}
+    amostras_por_grupo = 30
     start_time = time()
     for _ in range(amostras_por_grupo):
-
-        # 3 vezes a cada 5 segundos (será que era para dividir
-        # igualmente o tempo?)
-
-
-        # Colocar as 3 páginas pra buscar ao mesmo tempo
         pegar_pagina(net, clientes, tempos)
-
-
-        # ****************************************************************************
-
-        # Colocar as 3 páginas pra buscar ao mesmo tempo
         pegar_pagina(net, clientes, tempos)
-
-
-        # ***********************************************************
-
-        # Colocar as 3 páginas pra buscar ao mesmo tempo
         pegar_pagina(net, clientes, tempos)
-
-
         sleep(5)
-        now = time()
-        delta = now - start_time
-        if delta > args.time: # Esgotou o tempo limite
+        if time() - start_time > args.time:
             print("Teste encerrado")
             break
-        print("%.1fs left..." % (args.time - delta))
+        print(f"{args.time - (time() - start_time):.1f}s left...")
 
-
-    # =====================================================================
-
-
-    # Encerra pings
-    for ping in pings:
-        ping.terminate()
-
-    # Encerra as conexões tcp dos clientes
-    for cliente in iperf_clientes:
-        cliente.terminate()
-
-    for arquivo in iperf_arquivos:
-        arquivo.close()
-
-
-    # Encerra as conexões tcp do servidor (?)
+    for ping in pings: ping.terminate()
+    for c in iperf_clientes: c.terminate()
+    for f in iperf_arquivos: f.close()
     iperf_servidor.terminate()
     qmon.terminate()
-    for p in web:
-        p.terminate()
-
-    # Fecha rede
+    for p in web: p.terminate()
     net.stop()
-
-    # TODO: compute average (and standard deviation) of the fetch
-    # times.  You don't need to plot them.  Just note it in your
-    # README and explain.
-
-    local_salvar_bbr = os.path.join(args.dir, 'graficos', 'bbr',f'grafico-tempos-distribuicao-bbr-q{args.maxq}.png')
-    local_salvar_reno = os.path.join(args.dir, 'graficos', 'reno',f'grafico-tempos-distribuicao-reno-q{args.maxq}.png')
-
-    tempos['bbr'] = np.array(tempos['bbr'])
-    tempos['reno'] = np.array(tempos['reno'])
-
-    # Plotar gráfico e salvar
-    # plotar_tempos(tempos['bbr'], local_salvar_bbr)
-    # plotar_tempos(tempos['reno'], local_salvar_reno)
-
-
-    # Ensure that all processes you create within Mininet are killed.
-    # Sometimes they require manual killing.
     Popen("pgrep -f webserver.py | xargs kill -9", shell=True).wait()
 
 if __name__ == "__main__":
--- mininet-vagrant/bufferbloat/plot_throughput.py
+++ mininet-vagrant/bufferbloat/plot_throughput.py
@@ -1,3 +1,4 @@
+# plot_throughput.py
 import os
 import glob
 import argparse
@@ -14,13 +15,10 @@ parser.add_argument('--dir', '-d',
                     nargs='+')
 parser.add_argument('--bwnet',
                     help="Largura de banda do servidor em Mbits/s",
-                    required=True,
-                    )
-
+                    required=True)
 parser.add_argument('--out', '-o',
                     help="Arquivo de saída PNG para o gráfico",
                     default=None)
-
 args = parser.parse_args()
 
 def parse_vazao_com_tempo(fname):
@@ -35,29 +33,25 @@ def parse_vazao_com_tempo(fname):
                 intervalo = tokens[2]  # ex: "0.0-1.0"
                 start, end = map(float, intervalo.split('-'))
                 tempo_medio = (start + end) / 2
-
                 taxa = float(tokens[-2])
                 unidade = tokens[-1].lower()
                 if unidade == "bits/sec":
-                    taxa = taxa / 1e6
+                    taxa /= 1e6
                 elif unidade == "kbits/sec":
-                    taxa = taxa / 1e3
+                    taxa /= 1e3
                 elif unidade == "mbits/sec":
                     pass
                 elif unidade == "gbits/sec":
-                    taxa = taxa * 1e3
+                    taxa *= 1e3
                 else:
                     continue
-
                 tempos.append(tempo_medio)
                 vazoes.append(taxa)
             except:
                 continue
     return tempos, vazoes
 
-
 def agrupar_por_tempo(lista_de_series):
-    """Converte listas [(tempos, vazoes), ...] em dicionário tempo → lista de valores"""
     conjunto_tempos = set.intersection(*[set(t) for (t, _) in lista_de_series])
     agrupado = defaultdict(list)
     for t in sorted(conjunto_tempos):
@@ -66,28 +60,23 @@ def agrupar_por_tempo(lista_de_series):
             agrupado[t].append(valores[idx])
     return agrupado
 
-# Carrega todos os arquivos
 arquivos_bbr = []
 arquivos_reno = []
-
 for d in args.dir:
     padrao = os.path.join(d, "*-vazao.txt")
     arquivos = glob.glob(padrao)
-    arquivos_bbr.extend([f for f in arquivos if "bbr" in os.path.basename(f).lower()])
-    arquivos_reno.extend([f for f in arquivos if "reno" in os.path.basename(f).lower()])
-
+    arquivos_bbr += [f for f in arquivos if "bbr" in os.path.basename(f).lower()]
+    arquivos_reno += [f for f in arquivos if "reno" in os.path.basename(f).lower()]
 
 series_bbr = [parse_vazao_com_tempo(f) for f in arquivos_bbr]
 series_reno = [parse_vazao_com_tempo(f) for f in arquivos_reno]
 
-# Agrupar por tempo
 bbr_agrupado = agrupar_por_tempo(series_bbr)
 reno_agrupado = agrupar_por_tempo(series_reno)
 
-# Gerar trajetória fairness
-tempos_em_comum = sorted(bbr_agrupado.keys())
+tempos_comuns = sorted(bbr_agrupado.keys())
 fairness_pontos = []
-for t in tempos_em_comum:
+for t in tempos_comuns:
     if reno_agrupado[t] and bbr_agrupado[t]:
         fairness_pontos.append((
             np.mean(reno_agrupado[t]),
@@ -96,47 +85,43 @@ for t in tempos_em_comum:
 
 if len(fairness_pontos) < 2:
     print("[AVISO] Dados insuficientes para plotar trajetória de fairness.")
-    exit(0)  # ou continue se estiver dentro de um loop
+    exit(0)
 
 xs, ys = zip(*fairness_pontos)
 plt.figure(figsize=(6, 6))
 plt.plot(xs, ys, color='red', linewidth=1, marker='o')
 
 step = 10
-xs = xs[::step]
-ys = ys[::step]
-colors = cm.plasma(np.linspace(0, 1, len(xs)))
-# Trajetória com cor variando no tempo
-for i in range(len(xs) - 1):
-    plt.arrow(xs[i], ys[i],
-              xs[i + 1] - xs[i], ys[i + 1] - ys[i],
+xs_s = xs[::step]
+ys_s = ys[::step]
+colors = cm.plasma(np.linspace(0, 1, len(xs_s)))
+for i in range(len(xs_s) - 1):
+    plt.arrow(xs_s[i], ys_s[i],
+              xs_s[i + 1] - xs_s[i], ys_s[i + 1] - ys_s[i],
               color=colors[i],
               width=0.002, head_width=0.015, head_length=0.03,
               length_includes_head=True)
 
-# Pontos discretos ao longo da trajetória
-plt.scatter(xs, ys, c=colors, cmap='plasma', s=20)
-plt.scatter(xs[0], ys[0], color='blue', label='Início', zorder=5)
-plt.scatter(xs[-1], ys[-1], color='green', label='Fim', zorder=5)
+plt.scatter(xs_s, ys_s, c=colors, cmap='plasma', s=20)
+plt.scatter(xs_s[0], ys_s[0], color='blue', label='Início', zorder=5)
+plt.scatter(xs_s[-1], ys_s[-1], color='green', label='Fim', zorder=5)
 
+R = float(args.bwnet)
+n = len(series_reno)
+m = len(series_bbr)
+# agora considera n·x + m·y = R
+x_vals = np.linspace(0, R / n, 200)
+y_vals = (R - n * x_vals) / m
+plt.plot(x_vals, y_vals, 'k-', label=f'Uso total da banda (n·x + m·y = {R})')
 
-R = float(args.bwnet)  # largura de banda total da rede em Mbits/s
-
-# Linha de justiça: x = y
 plt.plot([0, R], [0, R], 'k--', label='Fair Share (x = y)')
-
-# Linha de eficiência total: x + y = R ⇒ y = R - x
-x_vals = np.linspace(0, R, 100)
-y_vals = R - x_vals
-plt.plot(x_vals, y_vals, 'k-', label=f'Uso total da banda (x + y = {R})')
-
-
 plt.xlabel("Throughput médio Reno (Mbits/s)")
 plt.ylabel("Throughput médio BBR (Mbits/s)")
 plt.title("Fairness")
 plt.legend()
 plt.grid(True)
 plt.axis('equal')
+
 if args.out:
     plt.savefig(args.out, bbox_inches='tight')
 else:
